9d67dca364bec3d6c63fbcdb28f4fbbdc5b5572b
Felix Magnus <felix.p.magnus@gmail.com>
2017-12-07 23:39:39 +0100

    solution for Day 5: Part 2

---
+++ 2017/05_02.js
@@ -0,0 +1,33 @@
+// read every line 
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('05_01_puzzle.txt')
+})
+
+let instructions = []
+
+// write every line into array
+lineReader.on('line', function (line) {
+    instructions.push(parseInt(line))
+})
+
+// after all lines are read iterate instructions
+lineReader.on('close', function () {
+	// save last index, current index and amount of steps
+	let lasti = 0
+	let i = 0
+	let steps = 0
+
+	// iterate as long as we are within the bounds of the instructions
+    while (i < instructions.length) {
+    	// save last index
+	    lasti = i
+	    // increment current index depending on instruction
+    	i += instructions[i]
+    	// if the last instruction was >= 3, decrease instruction where we just were by 1
+    	// else add 1 to instruction where we just were
+	    instructions[lasti] >= 3 ? instructions[lasti]-- : instructions[lasti]++
+	    // increase needed steps
+    	steps++
+    }
+    console.log("Needed " + steps + " steps.")
+})
