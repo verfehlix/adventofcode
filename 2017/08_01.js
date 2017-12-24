d26bc8483b877908497c775ec2cdd1771c9b7371
Felix <felix.p.magnus@gmail.com>
2017-12-24 14:29:42 +0100

    solution for Day 8: Part 1

---
+++ 2017/08_01.js
@@ -0,0 +1,69 @@
+// read every line 
+const lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('08_01_puzzle.txt')
+})
+
+const lines = []
+const registers = {}
+
+// parse every line
+lineReader.on('line', function (line) {
+    lines.push(line)
+})
+
+// after all lines are read, 
+lineReader.on('close', function () {
+    // register every register (lul)
+    lines.forEach(line => {
+        const regName1 = line.split(" ")[0]
+        const regName2 = line.split(" ")[4]
+
+        registers[regName1] = 0
+        registers[regName2] = 0
+
+    })
+
+    // iterate all the lines, check conditions and execute operations if possible
+    lines.forEach(line => {
+        // register to modify
+        const registerToModify = line.split(" ")[0]
+
+        // condition
+        const condition = line.split("if")[1].trim()
+
+        if (conditionPasses(condition)) {
+            // operation
+            const operation = line.split(" ")[1] === "inc" ? "+" : "-"
+            const value = parseInt(line.split(" ")[2])
+
+            const expression = "registers['" + registerToModify + "'] " + operation + "= " + parseInt(value)
+
+            eval(expression)
+        }
+    })
+
+    console.log(registers)
+
+    let biggestValue = 0
+    let registerName = ""
+
+    Object.keys(registers).forEach(function (key) {
+      let value = registers[key]
+      if(value > biggestValue){
+        biggestValue = value
+        registerName = key
+      }
+    })
+
+    console.log("The biggest value in any of the registers is: " + biggestValue + " in register " + registerName)
+})
+
+function conditionPasses(condition) {
+    const register = condition.split(" ")[0]
+    const operation = condition.split(" ")[1]
+    const value = condition.split(" ")[2]
+
+    const expression = registers[register] + " " + operation + " " + parseInt(value)
+
+    return eval(expression)
+}
